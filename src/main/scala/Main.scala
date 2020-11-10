b2e77264a67ab11bcdcee77b236ea80867ad5573
Abdelkabiir <abdelkabir.watil@gmail.com>
2020-11-10 21:36:12 +0100

    refactor: remove unused imports for now

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,4 @@
-import akka.actor.ActorSystem
-import akka.http.javadsl.server.Route
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods.POST
-import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpHeader, HttpRequest, HttpResponse, Uri}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
-import com.typesafe.config.ConfigFactory
-
-import scala.concurrent.{ExecutionContext, Future}
 
 object Main extends App {
-  implicit val system: ActorSystem = ActorSystem("passwordGeneratorServer")
 
 }
